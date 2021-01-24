fd1caf2cf2ce3119ab1fe603b397c96608847d21
Marcelo Pinheiro <marcelompinheiro@outlook.com>
2021-01-24 22:18:16 +0000

    fixing data

--- services/signup.service.js
+++ services/signup.service.js
@@ -1,6 +1,8 @@
 export async function signup(payload) { 
+
+    console.log(process.env.VERCEL_URL)
     
-    const response = await fetch(`${process.env.VERCEL_URL}/api/signup`, {
+    const response = await fetch(`/api/signup`, {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json'
